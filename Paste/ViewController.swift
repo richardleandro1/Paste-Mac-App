873e594842f26890a5f4e6ae4204b65f4698f231
richard leandro <richardp.leandro@gmail.com>
2022-08-28 04:15:02 -0300

    first commit

--- Paste/ViewController.swift
+++ Paste/ViewController.swift
@@ -7,6 +7,17 @@
 
 import Cocoa
 
+// MARK: - UI Effects
+// TODO: Step UI controls -> run app, call with shurtcut key "cmd + G"
+// TODO: Step UI -> create the effect 'upside' app
+
+// MARK: - Controls Effects
+// TODO: Step 1# create a function to take all data in ctrl when application is runned
+// TODO: Step 2# create a view with take all data in looping and show data in view
+// TODO: Step 3# access each data in view
+// TODO: Step 4# use the apple api for show each "quikly visualization" with each data type 
+
+
 class ViewController: NSViewController {
 
     override func viewDidLoad() {
@@ -20,7 +31,11 @@ class ViewController: NSViewController {
         // Update the view, if already loaded.
         }
     }
+    
+    func mainviewTestLayout(){
+        
+    }
 
-
+    //
 }
 
